3da42af35bd23acc93e564dd6a48c257793089d0
Anfal <ahassan2@mof.gov.kw>
2022-09-29 17:35:00 +0300

    finished the first part of the task only

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
 class Wallet:
     def __init__(self, money):
-        pass
+        self.money = money
+        
+        
 
-    def credit(amount):
-        pass
+    def credit(self,amount):
+        self.money += amount
+        print(f"you have : {self.money}")
+        
 
-    def debit(amount):
-        pass
+    def debit(self,amount):
+        self.money -= amount
+        print(f"you have : {self.money}")
+        
 
 
-wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
-print(wallet)
+# wallet = Wallet(6)
+# wallet = Wallet()  # This should default money inside the wallet to 0
+# print(wallet.money)
+# wallet.credit(2)
 
 
 class Person:
-    # Implement the code here
-    pass
+   def __init__(self, name, location,money ):
+        self.name= name
+        self.location= location
+        self.wallet=Wallet(money)
+    
+    def moveTo(point):
+        point = Person(location)
+        print(self.point)
 
 
-person = Person("Moh", 5, 50)
+
+
+# person = Person("Moh", 5, 50)
 
 
 class Vendor:
-    # implement Vendor!
-    pass
+#     # implement Vendor!
+#     pass
+    def __init__(self, name, location,money ):
+        super().__init__(name,location,money)
+        self.range = 5
+        self.price = 1
+    def sellTo(customer,number_of_icecream):
+        self.customer = customer
+        self.number_of_icecream = number_of_icecream
+        while range<5 and price>0:
+            print("their is only 3 icecream")
+# vendor = Vendor("Abdallah", 3, 6)
 
 
-vendor = Vendor("Abdallah", 3, 6)
+# class Customer:
+#     # implement Customer!
+#     pass
 
 
-class Customer:
-    # implement Customer!
-    pass
-
-
-customer = Customer("Abdallah", 3, 6)
+# customer = Customer("Abdallah", 3, 6)
