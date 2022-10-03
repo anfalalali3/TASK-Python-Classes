23bbd551a2802a0315efef40eb13e609659c78fd
Anfal <ahassan2@mof.gov.kw>
2022-10-04 00:03:35 +0300

    task barred after modification

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class Wallet:
-    def __init__(self, money):
+    def __init__(self, money=0):
         self.money = money
           
-
+    def __str__(self):
+        return f"This wallet has  {self.money}"
 
     def credit(self,amount):
         self.money += amount
@@ -16,37 +17,33 @@ class Wallet:
         
 
 
-# wallet = Wallet(6)
-# wallet = Wallet()  # This should default money inside the wallet to 0
-# print(wallet.money)
-# wallet.credit(2)
-
+wallet = Wallet(6)
+ 
+ 
 
 class Person:
     def __init__(self, name, location,money ):
         self.name= name
         self.location= location
-        self.money= Wallet(money)
+        self.wallet= Wallet(money)
 
     def moveTo(self, point):
-         point = Person(location)
-         print(self.point)
+         self.location = point
+    
+person = Person("Moh ", 5, 50)   
 
 class Vendor(Person):
 #     # implement Vendor!
 
-    def __init__(self, name, location,money,range,price):
+    def __init__(self, name, location,money):
         super().__init__(name,location,money)
-        self.range = range
-        self.price = price
-        range = 5
-        price = 1
+        self.range = 5
+        self.price = 1
+        
 
     def sellTo(self,customer,number_of_icecream):
-        self.customer = customer
-        self.number_of_icecream = number_of_icecream
-        self.moveTo(customer.location)
-        self.wallet.credit(self.price * number_of_icecream)
+        self.location = customer.location
+        self.wallet.credit(number_of_icecream*self.price)
         customer.wallet.debit(self.price * number_of_icecream)
         print(f"{number_of_icecream} were sold")
 
@@ -59,18 +56,19 @@ class Customer(Person):
 #     # implement Customer!
     def __init__(self, name, location,money ):
         super().__init__(name,location,money)
+
     def _is_in_range(self, vendor):
-        self.vendor = vendor
-        if abs(location - vendor.location)<= vendor.range:
-            print("the customer is in range")
-            return true
+        distance= vendor.location -self.location
+        if distance>vendor.range:
+            print("yeey!! you are in range")
+            return True
 
         else:
-            print("the customer is out of range")
-            return false
+            print("sorry you are out of range")
+            return False
+
     def _have_enough_money(self, vendor, number_of_icecreams):
-        self.vendor = vendor
-        self.number_of_icecreams = number_of_icecreams
+        
         if self.wallet.money >= (vendor.price * number_of_icecreams):
             print("you have enough money")
             return True 
@@ -79,17 +77,21 @@ class Customer(Person):
             return False 
     
     def request_icecream(self, vendor, number_of_icecreams):
-        self.vendor = vendor
-        self.number_of_icecreams = number_of_icecreams
-        if self._is_in_range and self._have_enough_money:
+        
+        if self._is_in_range(vendor) and self._have_enough_money(vendor,number_of_icecreams):
             print("request has been made")
-            vendor.sellTo(customer,number_of_icecreams)
+            vendor.sellTo(self,number_of_icecreams)
         
     def __str__(self):
         return f"you are the customer{super().__str__()}"
         
 
     
-vendor = Vendor("Anfal", 3, 6 )
-customer = Customer("Ali", 20, 6 )
-customer.request_icecream(vendor, 3)
+vendor_Anfal = Vendor("Anfal", 10, 10 )
+nearby_customer = Customer("Ali",12, 10)
+distant_customer = Customer("Adal",1000,500)
+nearby_customer.request_icecream(vendor_Anfal,10)
+print(nearby_customer.wallet.money)
+print(vendor_Anfal.wallet.money)
+print(vendor_Anfal.location)
+
